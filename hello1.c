d2347ab11bde7d9c2aacb89b08f979c9a9279d3d
jasminjoseph <jasminjohnjoseph1@gmail.com>
2018-09-10 13:17:55 -0500

    only a test

---
+++ hello1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello1.c
+ *
+ *    Description:  Trying out input and output for cprogram        
+ *
+ *        Version:  1.0
+ *        Created:  09/08/2018 09:07:40 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  JASMIN JOSEPH (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <stdio.h>
+
+void main() {
+//  char a[20];
+  int b = 0;
+    printf("Enter item:");
+    scanf ("%d",&b);
+    printf("The inputted item is : %d",b);
+
+}
